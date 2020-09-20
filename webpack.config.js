235b74747a1b1600550e4d557e579bcb23aab315
Oleg Tretyakov <ichimaru859@gmail.com>
2020-09-20 05:09:01 +0500

    add: @babel/polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ const filename = ext => isDev ? `bundle.[hash].${ext}` : `bundle.${ext}`
 module.exports = {
   context: path.resolve(__dirname, 'src'),
   mode: 'development',
-  entry: './index.js',
+  entry: ['@babel/polyfill','./index.js'],
   output: {
     filename: filename('js'),
     path: path.resolve(__dirname, 'dist')
